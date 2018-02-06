1d0b5f61981df6f3a4c8a9bdb31134ad7f060f6d
= <=>
2018-02-06 23:08:09 +0200

    Add Rest Controller Action calc_success

--- ShakespeareAPI/urls.py
+++ ShakespeareAPI/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from Shakespeare.views import calc_success
 
 urlpatterns = [
+    url(r'^calc_success$', calc_success ,name='calc_success'),
     path('admin/', admin.site.urls),
 ]
