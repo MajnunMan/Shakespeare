9f7a402ea8abf82e1de5aaf8eb89d735eb964cc4
= <=>
2018-02-07 20:36:43 +0200

    Add FRONT-END

---
+++ FRONT-END/js/script.js
@@ -0,0 +1,21 @@
+$("#d-form-js").submit(function(event){
+    // let category = $("#category-js").val();
+    // let text = $("#camp-text-js").val();
+    // console.log("category: " + category);
+    // console.log("text: " + text);
+    // $.ajax({
+    //    type: "GET",
+    //    url: "http://127.0.0.1:8000/calc_success",
+    //    data: {
+    //      category: category,
+    //      content: text
+    //    },
+    //    contentType: "application/json; charset=utf-8",
+    //    dataType: "json",
+    //    success: function (result) {
+    //        console.log(result);
+    //     }
+    //   });
+    // event.preventDefault();
+    alert("asdsa");
+});
