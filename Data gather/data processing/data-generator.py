88d5c816e6b9476f1f9be8a03a92cafb348175e7
Janmoppel <moppel.jan@gmail.com>
2018-02-08 22:39:06 +0200

    Upgraded data

--- Data gather/data processing/data-generator.py
+++ Data gather/data processing/data-generator.py
@@ -31,33 +31,36 @@ class JSONEncoder(json.JSONEncoder):
 
 
 def main():
-
     if __name__ == '__main__':
+        # Create CSV
         #process('food')
         #process('sport')
 
-        #df = pd.read_csv('sport-data-combined.csv', delimiter=',')
-        #data = df.sort_values(by='rating', ascending=False)
-        #data.to_csv('sport-data-combined.csv', encoding='utf-8')
+        # Order by rating
+        #ratingOrder('food.csv')
+        #ratingOrder('sport.csv')
 
-        #scaleRating("food-data-combined.csv")
-        scaleRating("sport-data-combined.csv")
+        # Normalize rating
+        #normalize("food.csv")
+        normalize("sport.csv")
 
 
 def process(category):
     # Save the retrieved tweets to file:
     dict_list = []
-    for tweet in query_tweets(category, limit=3000, begindate=dt.date(2016, 1, 1), poolsize=100, lang='en'):
+    for tweet in query_tweets(category, limit=3000, begindate=dt.date(2013, 1, 1), enddate=dt.date(2014, 1, 1),
+                              poolsize=100, lang='en'):
         # print(json.dumps(tweet, cls=JSONEncoder))
 
         # Eliminate unnecessary fields from tweet
-        optimised_data = optimiser(json.dumps(tweet, cls=JSONEncoder), unnecessary_field=['fullname', 'timestamp'])
+        optimised_data = optimiser(json.dumps(tweet, cls=JSONEncoder),
+                                   unnecessary_field=['fullname', 'timestamp'])
         dict_list.append(optimised_data)
 
     # Sorting tweets by rating (likes + replies + retweets)
     sorted_dict = sorted(dict_list, key=lambda d: d['rating'], reverse=True)
     timestr = time.strftime("%Y%m%d-%H%M%S")
-    dictToCSV(category +"-data" + timestr + ".csv", sorted_dict[0].keys(), sorted_dict)
+    dictToCSV(category + "-data" + timestr + ".csv", sorted_dict[0].keys(), sorted_dict)
 
     return
 
@@ -75,7 +78,6 @@ def optimiser(data, unnecessary_field=None):
 
 
 def dictToCSV(csv_file, csv_columns, dict_data):
-
     with codecs.open(csv_file, 'w', "utf-8") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
         writer.writeheader()
@@ -84,17 +86,22 @@ def dictToCSV(csv_file, csv_columns, dict_data):
     return
 
 
-def scaleRating(csv):
-
+def normalize(csv):
+    # Normalization
     data = pd.read_csv(csv)
     rating_norm = preprocessing.MinMaxScaler().fit_transform(data[['rating']])
     data['rating_norm'] = rating_norm
     data.to_csv(csv)
-    #X = np.asanyarray(data[['rating']])
-    #print(np.isfinite(X.sum()))
-    #print(np.isfinite(X).all())
 
     return
 
+
+def ratingOrder(csv):
+    # Sort combined CSV by rating
+    df = pd.read_csv(csv, delimiter=',')
+    data = df.sort_values(by='rating', ascending=False)
+    data.to_csv(csv, encoding='utf-8')
+
+    return
 
 main()
